3448a3f980fe48df5750a211e673136664db00d8
erfangchen <eukHGfhS68hvfx>
2021-02-17 20:59:02 -0500

    wip

--- src/main/kotlin/com/starburst/starburst/models/ReservedNames.kt
+++ src/main/kotlin/com/starburst/starburst/models/ReservedNames.kt
@@ -5,5 +5,9 @@ object ReservedNames {
     const val CostOfGoodsSold = "CostOfGoodsSold"
     const val GrossProfit = "GrossProfit"
     const val OperatingIncome = "OperatingIncome"
+    const val OperatingExpense = "OperatingExpense"
+    const val NonOperatingExpense = "NonOperatingExpense"
+    const val TaxExpense = "TaxExpense"
+    const val InterestExpense = "InterestExpense"
     const val NetIncome = "NetIncome"
 }
--- src/main/kotlin/com/starburst/starburst/models/builders/GenericModelBuilder.kt
+++ src/main/kotlin/com/starburst/starburst/models/builders/GenericModelBuilder.kt
@@ -1,14 +1,65 @@
 package com.starburst.starburst.models.builders
 
+import com.starburst.starburst.models.Item
 import com.starburst.starburst.models.Model
+import com.starburst.starburst.models.ReservedNames.CostOfGoodsSold
+import com.starburst.starburst.models.ReservedNames.GrossProfit
+import com.starburst.starburst.models.ReservedNames.InterestExpense
+import com.starburst.starburst.models.ReservedNames.NetIncome
+import com.starburst.starburst.models.ReservedNames.NonOperatingExpense
+import com.starburst.starburst.models.ReservedNames.OperatingExpense
+import com.starburst.starburst.models.ReservedNames.OperatingIncome
+import com.starburst.starburst.models.ReservedNames.Revenue
+import com.starburst.starburst.models.ReservedNames.TaxExpense
 
 class GenericModelBuilder {
 
     /**
-     * [createModel] creates the skeleton of a model
+     * [createModel] creates the skeleton of a basic model
+     * this doesn't have to be the only skeleton model available
      */
     fun createModel(): Model {
-        TODO()
+        return Model(
+            items = listOf(
+                Item(
+                    name = Revenue,
+                    expression = "0.0"
+                ),
+                Item(
+                    name = CostOfGoodsSold,
+                    expression = "0.0"
+                ),
+                Item(
+                    name = GrossProfit,
+                    expression = "$Revenue - $CostOfGoodsSold"
+                ),
+                Item(
+                    name = OperatingExpense,
+                    expression = "0.0"
+                ),
+                Item(
+                    name = OperatingIncome,
+                    expression = "$GrossProfit - $OperatingExpense"
+                ),
+                Item(
+                    name = NonOperatingExpense,
+                    expression = "0.0"
+                ),
+                Item(
+                    name = InterestExpense,
+                    expression = "0.0"
+                ),
+                Item(
+                    name = TaxExpense,
+                    expression = "0.0"
+                ),
+                Item(
+                    name = NetIncome,
+                    expression = "$OperatingIncome - $NonOperatingExpense - $InterestExpense - $TaxExpense"
+                )
+            ),
+            periods = 5
+        )
     }
 
 }
