978b890391101cfbea646bfeccb1270365113d7f
erfangchen <eukHGfhS68hvfx>
2021-02-23 15:24:33 -0500

    wip

--- src/main/kotlin/com/starburst/starburst/models/translator/subtypes/FixedCostTranslator.kt
+++ src/main/kotlin/com/starburst/starburst/models/translator/subtypes/FixedCostTranslator.kt
@@ -6,7 +6,7 @@ import com.starburst.starburst.spreadsheet.Cell
 class FixedCostTranslator(private val ctx: ResolverContext): FormulaTranslator {
     override fun translateFormula(cell: Cell): Cell {
         val item = cell.item
-        val fixedCost = item?.fixedCost ?: error("a fixed cost must be specified")
+        val fixedCost = item.fixedCost ?: error("a fixed cost must be specified")
         return cell.copy(
             formula = "${fixedCost.cost}",
             dependentCellNames = emptyList()
--- src/main/kotlin/com/starburst/starburst/models/translator/subtypes/PercentOfRevenueTranslator.kt
+++ src/main/kotlin/com/starburst/starburst/models/translator/subtypes/PercentOfRevenueTranslator.kt
@@ -7,7 +7,7 @@ import com.starburst.starburst.spreadsheet.Cell
 class PercentOfRevenueTranslator(private val ctx: ResolverContext) : FormulaTranslator {
     override fun translateFormula(cell: Cell): Cell {
         val period = cell.period
-        val percentOfRevenue = cell.item?.percentOfRevenue?.percentOfRevenue ?: 0.0
+        val percentOfRevenue = cell.item.percentOfRevenue?.percentOfRevenue ?: 0.0
         val revenueCellName = "${Revenue}_Period$period"
         return cell.copy(
             formula = "$revenueCellName * $percentOfRevenue",
