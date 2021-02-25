7f80ca05928b4bb872647938c189d86535edee0b
erfangchen <eukHGfhS68hvfx>
2021-02-24 20:06:45 -0500

    wip

---
+++ src/main/kotlin/com/starburst/starburst/authn/AuthenticationFilter.kt
@@ -0,0 +1,39 @@
+package com.starburst.starburst.authn
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class AuthenticationFilter(private val jwtValidator: JwtValidator): OncePerRequestFilter() {
+
+    private val log = LoggerFactory.getLogger(AuthenticationFilter::class.java)
+
+    override fun doFilterInternal(servletRequest: HttpServletRequest, servletResponse: HttpServletResponse, chain: FilterChain) {
+        if (servletRequest.method == "OPTIONS") {
+            doFilter(servletRequest, servletResponse, chain)
+            return
+        }
+
+        val authorization = servletRequest.getHeader(HttpHeaders.AUTHORIZATION)
+        val accessToken = extractAccessToken(authorization)
+        try {
+            jwtValidator.decodeAndVerify(accessToken)
+            doFilter(servletRequest, servletResponse, chain)
+        } catch (e: Exception) {
+            error(e)
+        }
+    }
+
+    private fun extractAccessToken(authorization: String): String {
+        try {
+            return authorization.replaceFirst(("^Bearer ").toRegex(), "")
+        } catch (e: Exception) {
+            throw error(e)
+        }
+    }
+}
---
+++ src/main/kotlin/com/starburst/starburst/authn/JwtValidator.kt
@@ -0,0 +1,47 @@
+package com.starburst.starburst.authn
+
+import com.auth0.jwk.JwkException
+import com.auth0.jwk.JwkProvider
+import com.auth0.jwk.JwkProviderBuilder
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTVerificationException
+import com.auth0.jwt.interfaces.DecodedJWT
+import org.springframework.stereotype.Service
+import java.security.interfaces.RSAPublicKey
+import java.util.concurrent.TimeUnit
+
+@Service
+class JwtValidator {
+
+    private val issuer = "https://ease-wealth.us.auth0.com/"
+
+    private val jwkProvider: JwkProvider = JwkProviderBuilder(issuer)
+        .cached(5, 1, TimeUnit.HOURS)
+        .build()
+
+    fun decodeAndVerify(token: String): DecodedJWT {
+        val decodedJWT = JWT.decode(token)
+        //
+        // find the kid from jwkProvider, use that to construct the public key needed
+        //
+        val kid = decodedJWT.keyId
+        try {
+            val jwtVerifier = jwtVerifierForKid(kid)
+            return jwtVerifier.verify(decodedJWT)
+        } catch (e: JwkException) {
+            throw error(e)
+        } catch (e: JWTVerificationException) {
+            throw error(e)
+        }
+
+    }
+
+    private fun jwtVerifierForKid(kid: String): JWTVerifier {
+        val jwk = jwkProvider.get(kid)
+        val publicKey = jwk.publicKey as RSAPublicKey
+        return JWT.require(Algorithm.RSA256(publicKey, null)).withIssuer(issuer).acceptLeeway(0).build()
+    }
+
+}
