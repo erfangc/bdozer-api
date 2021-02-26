76704d326016e6067213c85eba2b57a0e1434e9d
erfangchen <eukHGfhS68hvfx>
2021-02-25 22:49:45 -0500

    wip

--- src/main/kotlin/com/starburst/starburst/authn/AuthenticationFilter.kt
+++ src/main/kotlin/com/starburst/starburst/authn/AuthenticationFilter.kt
@@ -3,11 +3,12 @@ package com.starburst.starburst.authn
 import com.auth0.jwt.JWT
 import com.auth0.jwt.interfaces.DecodedJWT
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.starburst.starburst.ApiError
 import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import java.lang.RuntimeException
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -28,8 +29,8 @@ class AuthenticationFilter(
             return
         }
 
-        val accessToken = extractJwtToken(servletRequest)
         try {
+            val accessToken = extractJwtToken(servletRequest)
             jwtValidator.decodeAndVerify(accessToken)
             doFilter(servletRequest, servletResponse, chain)
         } catch (e: Exception) {
@@ -38,7 +39,8 @@ class AuthenticationFilter(
     }
 
     fun error(resp: HttpServletResponse, e:Exception) {
-        resp.addHeader("Access-Control-Allow-Origin", "*")
+        resp.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        resp.addHeader(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON.toString())
         resp.status = 401
         val apiError = ApiError(message = "cannot authenticate the request, underlying error ${e.message}")
         val body = objectMapper.writeValueAsString(apiError)
@@ -53,11 +55,7 @@ class AuthenticationFilter(
 
         fun extractJwtToken(req: HttpServletRequest): String {
             val header = req.getHeader(HttpHeaders.AUTHORIZATION)
-            try {
-                return header.replaceFirst(("^Bearer ").toRegex(), "")
-            } catch (e: Exception) {
-                throw error(e)
-            }
+            return header.replaceFirst(("^Bearer ").toRegex(), "")
         }
 
     }
