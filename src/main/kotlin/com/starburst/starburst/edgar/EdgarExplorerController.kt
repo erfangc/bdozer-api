d3084a77ba45cfe8ddf34eb7b6651f4f8db9c24a
Erfang Chen <erfangc@gmail.com>
2021-03-05 12:12:16 -0500

    wip

--- src/main/kotlin/com/starburst/starburst/edgar/EdgarExplorerController.kt
+++ src/main/kotlin/com/starburst/starburst/edgar/EdgarExplorerController.kt
@@ -2,10 +2,15 @@ package com.starburst.starburst.edgar
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.module.kotlin.treeToValue
+import com.starburst.starburst.edgar.factbase.ingestor.FilingIngestor
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.BasicHttpEntity
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+import java.util.concurrent.Executors
 
 /**
  * This REST Controller basically is a pass through to the SEC's own Elasticsearch
@@ -18,8 +23,29 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("public/edgar-explorer")
 class EdgarExplorerController(
     private val http: HttpClient,
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val filingIngestor: FilingIngestor
 ) {
+
+    data class EdgarFilingMetadata(
+        val ciks: List<String> = emptyList(),
+        val period_ending: LocalDate,
+        val root_form: String? = null,
+        val file_num: List<String> = emptyList(),
+        val display_names: List<String> = emptyList(),
+        val sequence: String? = null,
+        val biz_states: List<String> = emptyList(),
+        val sics: List<String> = emptyList(),
+        val form: String,
+        val adsh: String,
+        val biz_locations: List<String> = emptyList(),
+        val file_date: LocalDate,
+        val file_type: String? = null,
+        val file_description: String? = null,
+        val inc_states: List<String> = emptyList()
+    )
+
+    private val executor = Executors.newCachedThreadPool()
 
     @GetMapping("entities", produces = ["application/json"])
     fun searchEntities(@RequestParam term: String): JsonNode {
@@ -39,7 +65,7 @@ class EdgarExplorerController(
         val entity = BasicHttpEntity()
 
         // pad with leading 0 to make 10 digits
-        val paddedCik = (0 until (10 - cik.length)).joinToString("") {"0"}  + cik
+        val paddedCik = (0 until (10 - cik.length)).joinToString("") { "0" } + cik
 
         entity.content = """
                 {
@@ -63,5 +89,34 @@ class EdgarExplorerController(
         httpPost.releaseConnection()
 
         return jsonNode
+    }
+
+    @PostMapping("bootstrap-filing-entity")
+    fun bootstrapFilingEntity(@RequestParam cik: String): Unit {
+        //
+        // Find the most recent 4 10-Qs and the most recent 10K and ingest those
+        //
+        val hits = (searchFilings(cik).at("/hits/hits") as ArrayNode).map {
+            objectMapper.treeToValue<EdgarFilingMetadata>(
+                it.at("/_source")
+            )!!
+        }
+        val tenKs = hits.filter { it.form == "10-K" }.sortedByDescending { it.period_ending }
+        val tenQs = hits.filter { it.form == "10-Q" }.sortedByDescending { it.period_ending }
+        val recent10Ks = tenKs.first()
+        val recent10Qs = tenQs.subList(0, 4)
+
+        executor.submit {
+            filingIngestor.ingestFiling(
+                cik = cik,
+                adsh = recent10Ks.adsh
+            )
+            for (recent10Q in recent10Qs) {
+                filingIngestor.ingestFiling(
+                    cik = cik,
+                    adsh = recent10Q.adsh
+                )
+            }
+        }
     }
 }
