6d032231315ad2b83d91ab39125d2af7d6ade932
Erfang Chen <erfangc@gmail.com>
2021-04-28 16:15:12 -0400

    wip

--- src/main/kotlin/com/bdozer/stockanalysis/excel/StockAnalysisExcelDownloaderController.kt
+++ src/main/kotlin/com/bdozer/stockanalysis/excel/StockAnalysisExcelDownloaderController.kt
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin
-@RequestMapping("public/stock-analyzer")
+@RequestMapping("public/stock-analyses")
 class StockAnalysisExcelDownloaderController(
     private val stockAnalysisExcelDownloader: StockAnalysisExcelDownloader
 ) {
 
-    @GetMapping("{id}/download")
+    @GetMapping("{id}/excel-download")
     fun download(@PathVariable id: String): HttpEntity<ByteArray> {
         return stockAnalysisExcelDownloader.download(id)
     }
