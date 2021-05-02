dbdc7cb8d3eb60685308d5bc45b1ecc522d5863f
Erfang Chen <erfangc@gmail.com>
2021-05-02 09:58:15 -0400

    wip

--- bdozer-api-factbase/src/main/kotlin/com/bdozer/api/factbase/BdozerFactbaseApplication.kt
+++ bdozer-api-factbase/src/main/kotlin/com/bdozer/api/factbase/BdozerFactbaseApplication.kt
@@ -14,32 +14,58 @@ fun main(args: Array<String>) {
 
     val cfg = AppConfiguration()
 
+    /*
+    Domain object construction code
+     */
     val httpClient = cfg.httpClient()
     val secFilingFactory = cfg.secFilingFactory(httpClient)
     val mongoClient = cfg.mongoClient()
     val mongoDatabase = cfg.mongoDatabase(mongoClient)
     val filingIngestor = FilingIngestor(mongoDatabase, secFilingFactory)
 
+    /*
+    AMQP infrastructure init code
+     */
     val connectionFactory = cfg.connectionFactory()
     val connection = connectionFactory.newConnection()
     val channel = connection.createChannel()
 
+    /*
+    Reminder MQ queue declaration is idempotent
+     */
+    channel.queueDeclare(
+        QUEUE_NAME,
+        // durable
+        true,
+        // not-exclusive to this connection
+        false,
+        // do not auto delete queue
+        false,
+        null
+    )
+
+    /*
+    Register shutdown hooks to safely stop the worker
+     */
     Runtime.getRuntime().addShutdownHook(Thread {
         mongoClient.close()
         channel.close()
         connection.close()
     })
 
-    channel.queueDeclare(
-        QUEUE_NAME,
-        false,
-        false,
-        false,
-        null
-    )
-
     val deliverCallback = DeliverCallback { _, message ->
+        // TODO implement business logic here
         log.info(message?.body?.decodeToString())
+
+        /*
+        ack the message ONLY after the work is complete, explicit ack is enabled
+        this prevents too many messages from being processed by a given worker
+         */
+        channel.basicAck(message.envelope.deliveryTag, false)
     }
-    channel.basicConsume(QUEUE_NAME, true, deliverCallback, CancelCallback {  })
+
+    /*
+    Start the consumer code
+     */
+    channel.basicConsume(QUEUE_NAME, false, deliverCallback, CancelCallback {  })
 }
