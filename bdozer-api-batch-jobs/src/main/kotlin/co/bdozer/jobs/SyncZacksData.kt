8d3217349c6a4531030a5c8e57c7e9f5d57226f6
Erfang Chen <erfangc@gmail.com>
2022-05-13 13:50:51 -0400

    added Dockerfile

--- bdozer-api-batch-jobs/src/main/kotlin/co/bdozer/jobs/SyncZacksData.kt
+++ bdozer-api-batch-jobs/src/main/kotlin/co/bdozer/jobs/SyncZacksData.kt
@@ -1,9 +1,13 @@
 package co.bdozer.jobs
 
 import co.bdozer.libraries.zacks.ZacksTableSyncer
+import org.slf4j.LoggerFactory
 import kotlin.system.exitProcess
 
+private val log = LoggerFactory.getLogger("Main")
 fun main() {
+    log.info("Synchronizing Zacks data")
     ZacksTableSyncer.syncTables()
+    log.info("Finished synchronizing Zacks data")
     exitProcess(0)
 }
